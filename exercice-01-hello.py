de800c16b9d47da18055d647a1a27edf183c542b
youcefyn8 <youcef.nemmiche@gmail.com>
2022-02-28 16:02:31 +0100

    Création du repo
    Ajout des premiers chapitre de cours.
    Ajout des exo correspondants

---
+++ exercice-01-hello.py
@@ -0,0 +1,12 @@
+# exercice-01-hello.py
+
+# exo 1.1
+# Affichez le message "programme démarré" en utilisant des doubles quotes
+
+# réponse 1.1
+print("hello")
+# exo 1.2
+# Affichez le message "programme terminé" en utilisant des simples quotes
+print("coucou tout le monde")
+# réponse 1.2
+
---
+++ variable.py
@@ -0,0 +1,36 @@
+nombre = 123 #nombre entier 
+print(nombre)
+
+nombre = 3.14 #nombre flotent 
+print(nombre)
+
+nombre = "foo bar baz"
+print(nombre) 
+
+text = "lorem ipsum"
+print(text)
+
+is_day = True
+print(is_day)
+
+has_accepted_ula= False
+print(has_accepted_ula)
+
+has_accepted_ula= None
+print(has_accepted_ula)
+
+html_code = '<h1>titre de mon blog </h1>'
+print(html_code)
+
+nickname = 'john \"Dead\" doe'
+nickname = 'john O\'Connor'
+print(nickname)
+
+multiline_text = "\\foo\nbar\nbaz"
+print (multiline_text)
+
+multiline_text = """'foo'
+"bar"
+baz"""
+print(multiline_text)
+
